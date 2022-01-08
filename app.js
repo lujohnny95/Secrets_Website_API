09fc069a4412dcd0d799b554ba4d0f95f42b6fa3
lujohnny95 <lu_johnny95@yahoo.com>
2022-01-08 15:28:32 +0000

    first commit: with Bcrypt Hash & Salt

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion 6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 8;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register")
+});
+
+app.post("/register", (req, res) =>{
+    
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    });
+
+});
+
+app.post("/login", (req, res) => {
+     
+    User.findOne({ email: req.body.username }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                bcrypt.compare(req.body.password, hash, (err, result) => {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
+            }
+        }
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log("Listening at port 3000")
+});
